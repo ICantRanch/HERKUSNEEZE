6ef6583a3b77f89904cabeaa9657637deff6093a
ICantRanch <hubertkula18@gmail.com>
2026-02-13 17:40:26 -0500

    Setup Git LFS and changed segmentation model loading to default to english if model is not ideal model is not available

--- src/atlas6/sentenceSegmentation.py
+++ src/atlas6/sentenceSegmentation.py
@@ -9,9 +9,10 @@ def parse_text_to_sentence(text, lang):
 
     try:
         model = langTable[lang]['model']
+        nlp = spacy.load('Segmentation Models/%s' % model)
     except:
         model = 'english'
+        nlp = spacy.load('Segmentation Models/%s' % model)
 
-    nlp = spacy.load('Segmentation Models/%s' % model)
     doc = nlp(text)
     return [sent.text for sent in list(doc.sents)]
